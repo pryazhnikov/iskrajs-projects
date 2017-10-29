4efb7cd4a9d76db18db82adb45b1ea3b0215ea9a
Victor Pryazhnikov <victor@pryazhnikov.com>
2017-10-29 12:57:00 +0300

    Distance meter refactoring + source file rename

--- projects/distance-meter.js
+++ projects/distance-meter.js
@@ -10,29 +10,60 @@ const PIN_INPUT_ULTRASONIC_ECHO = P13;
 // Остальные настройки
 const DISTANCE_MEASURE_INTERVAL_TIME_MS = 500;
 
+// Контекст работы со схемой
+let RangeScheme = {
+  '_isEnabled': false,
+
+  '_updateCallback': null,
+
+  '_updateIntervalId': null,
+
+  'isEnabled': function () {
+    return this._isEnabled;
+  },
+
+  'toggleEnabled': function () {
+    this._isEnabled = !this._isEnabled;
+    this._processCallback();
+  },
+
+  'init': function (callback) {
+    this._updateCallback = callback;
+    this._processCallback();
+  },
+
+  '_processCallback': function () {
+    if (this._isEnabled && this._updateCallback) {
+      this._updateIntervalId = setInterval(this._updateCallback,  DISTANCE_MEASURE_INTERVAL_TIME_MS);
+    } else if (!this._isEnabled && this._updateIntervalId) {
+      clearInterval(this._updateIntervalId);
+      this._updateIntervalId = null;
+    }
+  }
+};
+
 // Кнопка для глобального включения / выключения
 var isSchemeEnabled = true;
 var $toggleButton = require('@amperka/button')
   .connect(PIN_STATUS_BUTTON);
 
 function toggleSchemaStatus() {
-  isSchemeEnabled = !isSchemeEnabled;
-  console.log("Button click triggers new schema status: ", isSchemeEnabled);
+  RangeScheme.toggleEnabled();
+  console.log("Button click triggers new schema status: ", RangeScheme.isEnabled());
 }
 $toggleButton.on('click', toggleSchemaStatus);
 
-
+// Работа с дальномером
 var $sonicSensor = require('@amperka/ultrasonic')
   .connect({
     trigPin : PIN_INPUT_ULTRASONIC_TRIGGER,
     echoPin : PIN_INPUT_ULTRASONIC_ECHO
   });
 
-
 function main() {
   "use strict";
-  if (!isSchemeEnabled) {
-    console.log("The schema is disabled");
+  if (!RangeScheme.isEnabled()) {
+    console.log("The scheme is disabled");
     return;
   }
 
@@ -63,4 +94,5 @@ function main() {
 
 }
 
-setInterval(main,  DISTANCE_MEASURE_INTERVAL_TIME_MS);
+
+RangeScheme.init(main);
