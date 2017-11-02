21beca78117fe13485d7b1147b3cf886f2df7606
Victor Pryazhnikov <victor@pryazhnikov.com>
2017-11-02 21:39:39 +0300

    Distance meter: jsdoc + let/const using + measurement period decrease

--- projects/distance-meter.js
+++ projects/distance-meter.js
@@ -1,24 +1,42 @@
 /**
- * Этот проект для измерения расстояния
+ * Проект: датчик измерения расстояния
+ *
+ * Суть:
+ * Интенсивность лампочки зависит от расстояния, измеренного дальномером.
+ * Чем больше измеренное расстояние, тем менее ярко светит лампа.
  */
 
 // Настройки подключения датчиков
+
+/** Пин кнопки включения/выключения */
 const PIN_STATUS_BUTTON = P0;
+/** Пин лампочки */
 const PIN_DISTANCE_LIGHT = A0;
+/** Пара пинов для подключения дальномера  */
 const PIN_INPUT_ULTRASONIC_TRIGGER = P12;
 const PIN_INPUT_ULTRASONIC_ECHO = P13;
 
 // Остальные настройки
-const DISTANCE_MEASURE_INTERVAL_TIME_MS = 500;
+
+/** Время между снятием измерений с дальномера (в миллисекундах) */
+const DISTANCE_MEASURE_INTERVAL_TIME_MS = 250;
+
+// ====================
+// Основной код
+// ====================
 
 // "Высокоуровневый" объект для работы со схемой
-let RangeScheme = {
+const RangeScheme = {
+  // Работает ли схема изначально
   '_isEnabled': false,
 
+  // Callback, вызываемый при получении новых измерений
   '_updateCallback': null,
 
+  // Callback, вызываемый при включении/выключении схемы
   '_enabledChangeCallback': null,
 
+  // Идентификатор таймера, запускаемого по 
   '_updateIntervalId': null,
 
   'isEnabled': function () {
@@ -61,13 +79,15 @@ let RangeScheme = {
 };
 
 // Кнопка для глобального включения / выключения
-var $toggleButton = require('@amperka/button')
+const $toggleButton = require('@amperka/button')
   .connect(PIN_STATUS_BUTTON);
 $toggleButton.on('click', function () { RangeScheme.toggleEnabled(); });
 
-var $distanceLight = require('@amperka/led')
+// Лампочка
+const $distanceLight = require('@amperka/led')
   .connect(PIN_DISTANCE_LIGHT);
 
+// Действия при включении/выключении схемы
 RangeScheme.setEnabledChangeCallback(
   function (newEnabledValue) {
     console.log("The New scheme enabled value: ", newEnabledValue);
@@ -76,22 +96,33 @@ RangeScheme.setEnabledChangeCallback(
 );
 
 // Работа с дальномером
-var $sonicSensor = require('@amperka/ultrasonic')
+const $sonicSensor = require('@amperka/ultrasonic')
   .connect({
     trigPin : PIN_INPUT_ULTRASONIC_TRIGGER,
     echoPin : PIN_INPUT_ULTRASONIC_ECHO
   });
 
+/**
+ * Нормировочная функция для получения яркости статусной лампы.
+ * Основная идея - чем больше измеренное расстояние тем менее ярко светит лампа.
+ * Нормировка нужна для того чтобы изменения на маленьких расстояниях влияли сильнее,
+ * чем на больших.
+ * Например, разница интенсивностей между 20 и 40 сантиметрами должна быть больше,
+ * чем между 3 и 4 метрами.
+ *
+ * @param {number} x измеренное растояние
+ * @param {number} minValue минимальное расстояние (на нём должна быть максимальная яркость)
+ * @param {number} maxValue максимальное расстояние (на нём должна быть минимальная яркость)
+ */
 function getNormalizedBrightness(x, minValue, maxValue) {
-  let yMinValue = 1;
-  let yMaxValue = 0.1;
+  const yMinValue = 1;
+  const yMaxValue = 0.1;
 
   // f(x) = a / x + b
   // f(minValue) = yMinValue
   // f(maxValue) = yMaxValue
-
-  let a = minValue * maxValue * (yMinValue - yMaxValue) / (maxValue - minValue);
-  let b = (yMaxValue * maxValue - yMinValue * minValue) / (maxValue - minValue);
+  const a = minValue * maxValue * (yMinValue - yMaxValue) / (maxValue - minValue);
+  const b = (yMaxValue * maxValue - yMinValue * minValue) / (maxValue - minValue);
 
   let result = a / x + b;
   if (result > yMinValue) {
@@ -104,6 +135,10 @@ function getNormalizedBrightness(x, minValue, maxValue) {
   return result;
 }
 
+/**
+ * Функция, снимающая показания с дальномера и выставляющая интенсивность на лампе
+ * @param {number} measurePeriodTime время между снятием измерений
+ */
 function getNewDistanceValue(measurePeriodTime) {
   "use strict";
   if (!RangeScheme.isEnabled()) {
@@ -128,6 +163,7 @@ function getNewDistanceValue(measurePeriodTime) {
         }
       }
 
+      // Вывод отладочной информации
       let waitTimeMs = 1e3 * (timeFinish - timeStart);
       console.log(
         "Sonar value, mm:", sonarValue,
@@ -135,6 +171,8 @@ function getNewDistanceValue(measurePeriodTime) {
         "Brighness:", brightness
       );
 
+      // Контроль параметров: если мы слишком часто снимаем показания и они не успевают сниматься,
+      // то лушше будет увеличить внемя между снятиями показаний дальномера
       if (waitTimeMs > measurePeriodTime) {
         console.log("Sonar wait time is greater than measure period time", measurePeriodTime);
       }
@@ -144,5 +182,8 @@ function getNewDistanceValue(measurePeriodTime) {
 
 }
 
+// ====================
 // Запуск схемы
+// ====================
+
 RangeScheme.run(getNewDistanceValue);
